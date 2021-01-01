8112b56483695791e360650cdd7bda8be2e2996d
technoShopp <malinii.aus@gmail.com>
2021-01-01 20:12:38 +1000

    jasmineFramework- removing the direct connect

--- Config.js
+++ Config.js
@@ -1,7 +1,7 @@
 	var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 exports.config = {
 	
-		seleniumAddress:'http://localhost:4444/wd/hub',
+		//seleniumAddress:'http://localhost:4444/wd/hub',
 			specs:['spec1.js'],
 			onPrepare : function()
 			{
